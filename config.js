b46b429564d484d719789537354fb1e76fec4865
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-10-11 05:04:15 +0000

    db.url to uri

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 80,
     db: {
-	url: process.env.MONGODB_URL
+	uri: process.env.MONGODB_URL
     }
 }
