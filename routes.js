89f3a4ca9a413f292a9ef29138d2e37668b801d1
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-10-13 04:37:15 -0400

    Got rid of 10 limit on queries, added blue_lights descriptions

--- routes.js
+++ routes.js
@@ -50,16 +50,8 @@ module.exports = function(ctx) {
     // returns all the points 
     server.get('/points', (req, res, next) => {
 
-        let limit = parseInt(req.query.limit, 10) || 10, // default limit to 10 docs
-            skip  = parseInt(req.query.skip, 10) || 0, // default skip to 0 docs
-            query = req.query || {}
-
-        // remove skip and limit from query to avoid false querying
-        delete query.skip
-        delete query.limit
-
-        // find points and convert to array (with optional query, skip and limit)
-        collection.find(query).skip(skip).limit(limit).toArray()
+        // find points and convert to array 
+        collection.find(query).toArray()
             .then(docs => res.send(200, docs))
             .catch(err => res.send(500, err))
 
