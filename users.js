4789edfc36750ed841d573316005a3404a75af1f
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-10-14 03:16:40 -0400

    changed bcrypt to bcryptjs

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 'use strict'
-const bcrypt = require('bcrypt'),
+const bcrypt = require('bcryptjs'),
 	  randomWords = require('random-words'),
 	  fs = require('fs'),
 	  users = require("./secret"),
