5b00a76b51337831fb20291a990a57732d6f7935
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-11-02 17:56:18 -0400

    Added events collection (untested)

--- routes.js
+++ routes.js
@@ -10,7 +10,8 @@ module.exports = function(ctx) {
           server = ctx.server
 
     // assign collection to variable for further use
-    const collection = db.collection('points')
+    const point_collection = db.collection('points')
+    const event_collection = db.collection('events')
 
     /**
      * Create
@@ -30,7 +31,7 @@ module.exports = function(ctx) {
                         data.verified = true;
                         data.verified_by = req.body.email;
                         // insert one object into points collection
-                        collection.insertOne(data)
+                        point_collection.insertOne(data)
                             .then(doc => res.send(200, doc.ops[0]))
                             .catch(err => res.send(500, err))
                         next()
@@ -41,7 +42,7 @@ module.exports = function(ctx) {
                 })
             }else{
                 // insert one object into points collection
-                collection.insertOne(data)
+                point_collection.insertOne(data)
                     .then(doc => res.send(200, doc.ops[0]))
                     .catch(err => res.send(500, err))
 
@@ -51,6 +52,44 @@ module.exports = function(ctx) {
             res.send(400, validation.whyInvalidPoint(req.body))
         }
     })
+
+    server.post('/events', (req, res, next) => {
+        if(validation.validateEvent(req.body)){
+            // extract data from body and add timestamps
+            const data = Object.assign({}, req.body, {
+                created: new Date(),
+                updated: new Date(),
+                verified: false
+            })
+
+            if(req.params.key){
+                user.checkAPIKey(req.body.email, req.params.key).then((verified) => {
+                    if (verified) {
+                        data.verified = true;
+                        data.verified_by = req.body.email;
+                        // insert one object into points collection
+                        event_collection.insertOne(data)
+                            .then(doc => res.send(200, doc.ops[0]))
+                            .catch(err => res.send(500, err))
+                        next()
+                    }else{
+                        res.send(403, "API key does not match email. Entry was not added.")
+                        next()
+                    }
+                })
+            }else{
+                // insert one object into points collection
+                event_collection.insertOne(data)
+                    .then(doc => res.send(200, doc.ops[0]))
+                    .catch(err => res.send(500, err))
+
+                next()
+            }
+        }else{
+            res.send(400, validation.whyInvalidPoint(req.body))
+        }
+    })
+
 
     /**
      * Read
@@ -71,7 +110,7 @@ module.exports = function(ctx) {
     server.get('/points', (req, res, next) => {
 
         // find points and convert to array (with optional query, skip and limit)
-        collection.find(req.query || {}).toArray()
+        point_collection.find(req.query || {}).toArray()
             .then(docs => res.send(200, docs))
             .catch(err => res.send(500, err))
 
@@ -82,7 +121,29 @@ module.exports = function(ctx) {
     // returns a list of unique categories
     server.get('/points/categories', (req, res, next) => {
         // find points and convert to array (with optional query, skip and limit)
-        collection.distinct("category")
+        point_collection.distinct("category")
+            .then(docs => res.send(200, docs))
+            .catch(err => res.send(500, err))
+
+        next()
+    })
+
+    // returns all the events 
+    server.get('/events', (req, res, next) => {
+
+        // find points and convert to array (with optional query, skip and limit)
+        event_collection.find(req.query || {}).toArray()
+            .then(docs => res.send(200, docs))
+            .catch(err => res.send(500, err))
+
+        next()
+
+    })
+
+    // returns a list of unique categories
+    server.get('/events/categories', (req, res, next) => {
+        // find points and convert to array (with optional query, skip and limit)
+        event_collection.distinct("category")
             .then(docs => res.send(200, docs))
             .catch(err => res.send(500, err))
 
@@ -105,12 +166,12 @@ module.exports = function(ctx) {
     //                 let query = { _id: req.params.id },
     //                     body  = { $set: data },
     //                     opts  = {
-    //                         returnNewDocument: false,
+    //                         returnOriginal: false,
     //                         upsert: false
     //                     }
 
     //                 // find and update document based on passed in id (via route)
-    //                 collection.findOneAndUpdate(query, body, opts)
+    //                 point_collection.findOneAndUpdate(query, body, opts)
     //                     .then(doc => res.send(204))
     //                     .catch(err => res.send(500, err))
 
@@ -143,12 +204,47 @@ module.exports = function(ctx) {
                     let query = { _id: req.params.id },
                         body  = { $set: data },
                         opts  = {
-                            returnNewDocument: false,
+                            returnOriginal: false,
                             upsert: false
                         }
 
                     // find and update document based on passed in id (via route)
-                    collection.findOneAndUpdate(query, body, opts)
+                    point_collection.findOneAndUpdate(query, body, opts)
+                        .then(doc => res.send(204))
+                        .catch(err => res.send(500, err))
+
+                    next()
+                }else{
+                    res.send(403, "API key does not match email.")
+                    next()
+                }
+            })
+        }else{
+            res.send(403, "No API key or email provided to verify admin status.")
+            next()
+        }
+    })
+
+    server.put('/events/verified/:id/:email/:key', (req, res, next) => {
+        if(req.params.email && req.params.key){
+            user.checkAPIKey(req.params.email, req.params.key).then((verified) => {
+                if (verified) {
+                    const data = {
+                        updated: new Date(),
+                        verified: true,
+                        verified_by: req.params.email,
+                    }
+
+                    // build out findOneAndUpdate variables to keep things organized
+                    let query = { _id: req.params.id },
+                        body  = { $set: data },
+                        opts  = {
+                            returnOriginal: false,
+                            upsert: false
+                        }
+
+                    // find and update document based on passed in id (via route)
+                    event_collection.findOneAndUpdate(query, body, opts)
                         .then(doc => res.send(204))
                         .catch(err => res.send(500, err))
 
@@ -172,7 +268,29 @@ module.exports = function(ctx) {
             user.checkAPIKey(req.params.email, req.params.key).then((verified) => {
                 if (verified) {
                     // remove one document based on passed in id (via route)
-                    collection.findOneAndDelete({ _id: req.params.id })
+                    point_collection.findOneAndDelete({ _id: req.params.id })
+                        .then(doc => res.send(204))
+                        .catch(err => res.send(500, err))
+
+                    next()
+                }else{
+                    res.send(403, "API key does not match email.")
+                    next()
+                }
+            })
+        }else{
+            res.send(403, "No API key or email provided to verify admin status.")
+            next()
+        }
+
+    })
+
+    server.del('/events/:id/:email/:key', (req, res, next) => {
+        if(req.params.email && req.params.key){
+            user.checkAPIKey(req.params.email, req.params.key).then((verified) => {
+                if (verified) {
+                    // remove one document based on passed in id (via route)
+                    event_collection.findOneAndDelete({ _id: req.params.id })
                         .then(doc => res.send(204))
                         .catch(err => res.send(500, err))
 
